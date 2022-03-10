dfaac614965f3b845cb38b663f6dd54227ade87a
noobyysauraj <gitsauraj@gmail.com>
2022-03-10 09:17:52 +0000

    full_oppo6771_18611-user 8.1.0 O11019 1556007842 release-keys
    Signed-off-by: noobyysauraj <gitsauraj@gmail.com>

---
+++ BoardConfig.mk
@@ -0,0 +1,8 @@
+DEVICE_PATH := device/realme/RMX1831
+BOARD_VENDOR := realme
+
+# HIDL
+DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
+DEVICE_MATRIX_FILE := $(DEVICE_PATH)/compatibility_matrix.xml
+
+-include vendor/realme/RMX1831/BoardConfigVendor.mk
